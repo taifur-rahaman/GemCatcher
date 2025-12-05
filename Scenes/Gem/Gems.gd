b266643de9f0e1ac7e77b8e0b29c326d7ef10b81
taifur-rahaman <nmlsDv@gmail.com>
2025-12-05 16:50:13 +0600

    Paddle functionality added

--- Scenes/Gem/Gems.gd
+++ Scenes/Gem/Gems.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
 const falling_rate : float = 100.0
+var screen_width : float
+var screen_height : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var screen_size = get_viewport_rect().size
+	screen_height = screen_size.y
+	screen_width = screen_size.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += falling_rate * delta
 
-	if position.y > 648.00:
+	if position.y > screen_height:
 		print("Gem Off of the screen")
 		set_process(false)
 		queue_free() 
---
+++ Scenes/Paddle/Paddle.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+const moving_rate : float = 300.0
+
+var left_edge : float
+var right_edge : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	left_edge = get_viewport_rect().position.x
+	right_edge = get_viewport_rect().end.x
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if Input.is_action_pressed("move_left"):
+		#position.x -= moving_rate * delta
+		#if position.x < left_edge:
+			#position.x = get_viewport_rect().position.x
+	#elif Input.is_action_pressed("move_right"):
+		#position.x += moving_rate * delta
+		#if position.x > right_edge:
+			#position.x = get_viewport_rect().end.x
+			
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_left"):
+		position.x -= moving_rate * delta
+	elif Input.is_action_pressed("move_right"):
+		position.x += moving_rate * delta
+	position.x = clampf(position.x, left_edge, right_edge)
